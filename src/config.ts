3a87f417f27e4e94b2e5477c0308239566e5239a
evanbucket <evanbucket@gmail.com>
2023-05-18 10:01:10 -0600

    finished

--- src/config.ts
+++ src/config.ts
@@ -1,32 +1,142 @@
 export const NAVCONTENT = {
     TEXT: {
-
+        YOURNAME: "Your Name Here"
     },
     LINKS:{
         LINK1: {
             HREF: "/",
-            CONTENT: "HOOOOME",
+            CONTENT: "Home",
         },
         LINK2: {
-            HREF: "/",
-            CONTENT: "HOOOOME2"
+            HREF: "/projects/",
+            CONTENT: "Projects"
         }
     },
 }
 
 export const FOOTER = {
         TEXT: {
-            FEETER: "Hootskoot",
-            FEETER2: "lOER",
-            FEETER3: "lOFAGFD",
-            FEETER4: "lOAFDHADF",
-            FEETER5: "lOEADFGV",
-            FEETER6: "AFDHDAFER",
-            FEETER7: "AFGJHGR",
-            FEETER8: "gjsfgh",
-            FEETER9: "lgsadafgre",
+            FOOTHEAD1: "Company",
+            FOOTHEAD2: "Contact",
+            FOOTHEAD3: "Legal",
         },
         LINKS:{
-
+            LINK1: {
+            HREF: "/projects/",
+            CONTENT: "Projects"
+            },
+            LINK2: {
+            HREF: "/",
+            CONTENT: "Our Story"
+            },
+            LINK3: {
+            HREF: "/contact/",
+            CONTENT: "Email"
+            },
+            LINK5: {
+            HREF: "/legal/",
+            CONTENT: "Terms"
+            },
+            LINK6: {
+            HREF: "/legal/",
+            CONTENT: "Privacy"
+            },
         },
+}
+
+export const SECTION2 = {
+    TEXT: {
+        BIGHEADER: "Our Services",
+        HEADER1: "Service #1",
+        HEADER2: "Service #2",
+        HEADER3: "Service #3",
+        SERVICE1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu ultrices dolor, sit amet tempor quam. Nulla faucibus, elit eu mollis dictum, orci libero tempor ex, vitae aliquam ligula ipsum vel magna. Nullam consectetur diam mi, sit amet mollis dolor vestibulum sed. Phasellus pharetra",
+        SERVICE2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu ultrices dolor, sit amet tempor quam. Nulla faucibus, elit eu mollis dictum, orci libero tempor ex, vitae aliquam ligula ipsum vel magna. Nullam consectetur diam mi, sit amet mollis dolor vestibulum sed. Phasellus pharetra",
+        SERVICE3: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu ultrices dolor, sit amet tempor quam. Nulla faucibus, elit eu mollis dictum, orci libero tempor ex, vitae aliquam ligula ipsum vel magna. Nullam consectetur diam mi, sit amet mollis dolor vestibulum sed. Phasellus pharetra",
+    },
+
+    IMAGES: {
+        IMAGE:{
+            SRC: "../public/manonroof.jpg",
+            ALT: "A man sitting at a desk using a computer.",
+        }
+    },
+}
+
+export const HERO = {
+    TEXT: {
+        NAME: "Your Name Here"
+    },
+
+    IMAGES: {
+        HERO:{
+            SRC: "../public/manwithpc.jpg",
+            ALT: "Man at his computer.",
+        }
+    }
+}
+
+export const LEGAL = {
+    TEXT: {
+        TERMSHEADER: "Terms of Service",
+        PRIVACYHEADER: "Privacy Policy",
+        TERMS: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra aliquet eget sit amet. Vel pretium lectus quam id leo in vitae turpis. Augue neque gravida in fermentum et sollicitudin ac orci phasellus. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Pulvinar pellentesque habitant morbi tristique. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Vitae proin sagittis nisl rhoncus mattis rhoncus. Odio tempor orci dapibus ultrices in iaculis nunc sed augue. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Vestibulum morbi blandit cursus risus at ultrices mi Risus sed vulputate odio ut enim blandit volutpat maecenas volutpat. Lacinia quis vel eros donec ac odio tempor. Mauris ultrices eros in cursus turpis massa tincidunt dui ut. Elementum nibh tellus molestie nunc. Odio eu feugiat pretium nibh ipsum consequat nisl vel. Lacinia quis vel eros donec. Massa vitae tortor condimentum lacinia quis vel eros donec. Orci porta non pulvinar neque laoreet suspendisse interdum. Morbi enim nunc faucibus a pellentesque. Vel pretium lectus quam id leo in vitae. Volutpat odio facilisis mauris sit amet massa vitae tortor. Quis vel eros donec ac odio. Interdum consectetur libero id faucibus nisl tincidunt eget. In vitae turpis massa sed elementum tempus egestas. Duis at tellus at urna condimentum mattis pellentesque.",
+        PRIVACY: "Purus in massa tempor nec feugiat nisl pretium. Augue mauris augue neque gravida in. Sit amet dictum sit amet. Dolor sed viverra ipsum nunc aliquet bibendum. Sed risus pretium quam vulputate dignissim suspendisse in est. Viverra maecenas accumsan lacus vel facilisis volutpat est velit. Odio aenean sed adipiscing diam donec adipiscing tristique risus. Netus et malesuada fames ac turpis egestas sed tempus urna. Ultrices neque ornare aenean euismod elementum nisi quis. Suspendisse interdum consectetur libero id faucibus nisl. Diam donec adipiscing tristique risus nec feugiat. Amet aliquam id diam maecenas ultricies mi eget mauris. Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam. Eu lobortis elementum nibh tellus molestie nunc. Feugiat scelerisque varius morbi enim. Laoreet id donec ultrices tincidunt. Nunc vel risus commodo viverra maecenas accumsan. Aliquet nibh praesent tristique magna sit amet purus gravida. Elit duis tristique sollicitudin nibh sit. Purus in massa tempor nec feugiat nisl pretium fusce. Amet nisl purus in mollis nunc sed. Sit amet facilisis magna etiam tempor orci eu lobortis. Etiam non quam lacus suspendisse faucibus interdum. Elementum pulvinar etiam non quam lacus. Facilisis mauris sit amet massa vitae tortor condimentum. Venenatis tellus in metus vulputate eu. Vitae aliquet nec ullamcorper sit. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Et sollicitudin ac orci phasellus egestas tellus rutrum tellus. Duis ut diam quam nulla porttitor. Natoque penatibus et magnis dis parturient montes. Pellentesque pulvinar pellentesque habitant morbi tristique. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Nam libero justo laoreet sit. A pellentesque sit amet porttitor eget. Faucibus pulvinar elementum integer enim neque volutpat ac. Lacinia quis vel eros donec ac odio tempor orci. Donec adipiscing tristique risus nec feugiat in",
+    }
+}
+
+export const SECTION1 = {
+    TEXT: {
+        HEADER1:"BOOM",
+        HEADER2:"BOOM",
+        HEADER3:"BOOM",
+        CONTENT1:"HVFBAKJAAGET",
+        CONTENT2:"GAFKJGHVFBAKJAAGET",
+        CONTENT3:"GHVFBAKJAAGE",
+    }
+}
+
+export const PROJECTS = {
+    TEXT: {
+        TITLE:"Our Projects",
+        PROJECT1HEAD:"Project 1",
+        PROJECT2HEAD:"Project 2",
+        PROJECT3HEAD:"Project 3",
+        PROJECT4HEAD:"Project 4",
+        PROJECT1CONTENT:"a;ddflghfad;ghlfghfaggh",
+        PROJECT2CONTENT:"a;ddflghfad;ghlfghfaggh",
+        PROJECT3CONTENT:"a;ddflghfad;ghlfghfaggh",
+        PROJECT4CONTENT:"a;ddflghfad;ghlfghfaggh",
+    },
+
+    IMAGES: {
+
+        PROJECT1: {
+            SRC: "../public/anotherone.jpg",
+            ALT: "A man using a laptop at a desk"
+        },
+        PROJECT2: {
+            SRC: "../public/brosusingcomputer.jpg",
+            ALT: "Two women at a desk working together using their laptops"
+        },
+        PROJECT3: {
+            SRC: "../public/brosusingcomputer.jpg",
+            ALT: "Two women at a desk working together using their laptops"
+        },
+        PROJECT4: {
+            SRC: "../public/anotherone.jpg",
+            ALT: "A man using a laptop at a desk"
+        },
+        
+
+    }
+}
+
+export const CONTACT = {
+    TEXT: {
+        CONTACTUS:"Contact Us",
+        CONTACTCONTENT:"Aute magna irure deserunt veniam aliqua magna enim voluptate.",
+        CONTACTINFO:"(555)-555-5555 • emailus@ourcompany.com",
+
+    }
 }
