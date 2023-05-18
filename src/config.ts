0dd68e515671e7005ce8e47d1ae0933c0f3437f8
evanbucket <evanbucket@gmail.com>
2023-05-18 10:00:55 -0600

    finished

---
+++ src/config.ts
@@ -0,0 +1,32 @@
+export const NAVCONTENT = {
+    TEXT: {
+
+    },
+    LINKS:{
+        LINK1: {
+            HREF: "/",
+            CONTENT: "HOOOOME",
+        },
+        LINK2: {
+            HREF: "/",
+            CONTENT: "HOOOOME2"
+        }
+    },
+}
+
+export const FOOTER = {
+        TEXT: {
+            FEETER: "Hootskoot",
+            FEETER2: "lOER",
+            FEETER3: "lOFAGFD",
+            FEETER4: "lOAFDHADF",
+            FEETER5: "lOEADFGV",
+            FEETER6: "AFDHDAFER",
+            FEETER7: "AFGJHGR",
+            FEETER8: "gjsfgh",
+            FEETER9: "lgsadafgre",
+        },
+        LINKS:{
+
+        },
+}
